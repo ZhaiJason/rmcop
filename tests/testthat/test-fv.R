d46632a6d06cd2462a6d71fc6d92005961513f22
Jason Zhai <smicjasonzhai@163.com>
2022-12-29 01:20:32 +0000

    Removed outdated tests and function supplements.

--- tests/testthat/test-fv.R
+++
@@ -1,3 +0,0 @@
-test_that("fv() computes future value", {
-    expect_equal(fv(100, 0.05, 1), 105)
-})
--- tests/testthat/test-pv.R
+++
@@ -1,3 +0,0 @@
-test_that("pv() computes present value", {
-  expect_equal(pv(105, 0.05, 1), 100)
-})
