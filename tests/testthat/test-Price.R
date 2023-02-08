b5234708f632921e5c046d7454c10aa39d182915
Jason Zhai <smicjasonzhai@163.com>
2023-02-08 18:42:20 +0000

    Test added for price.option() function. Removed Trinomial.R

---
+++ tests/testthat/test-Price.R
@@ -0,0 +1,14 @@
+op <- option("european", "vanilla", "call", 20, 0.75)
+op.env <- option.env(S = 20, r = 0.03, q = 0.01, sigma = 0.05)
+
+test_that("mc works", {
+    expect_equal(price.option(op, op.env, n = 100000, steps = 10, all = FALSE, plot = FALSE),
+                 0.41,
+                 tolerance = 1e-1)
+})
+
+test_that("binomial works", {
+    expect_equal(price.option(op, op.env, n = 100, u = 1.2, d = 0.8, method = "binomial", all = FALSE),
+                 13.80,
+                 tolerance = 1e-2)
+})
